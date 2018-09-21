e76e1744aa8a8bce6cb19a7405da30dc1f070394
Matthew Jeng <mjeng@berkeley.edu>
2018-09-20 20:19:07 -0700

    #4

--- foobar.rb
+++ foobar.rb
@@ -4,6 +4,7 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
-    
+    a = a.map{ |e| e.to_i + 2 }.uniq.select{ |i| i%2==0 && i<10 }
+    a.sum
   end
 end
